7d1820a474118fed94663938e019502d535502e5
Manikanta Parameswarappa <manikantap036@gmail.com>
2026-04-07 14:32:53 +0530

    add-indexes-for-projects

--- src/main/java/com/manikanta/projects/lovable_backend/entity/Project.java
+++ src/main/java/com/manikanta/projects/lovable_backend/entity/Project.java
@@ -1,6 +1,5 @@
 package com.manikanta.projects.lovable_backend.entity;
 
-
 import jakarta.persistence.*;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
@@ -16,7 +15,17 @@ import java.time.Instant;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-@Table(name = "projects")
+@Table(name = "projects",
+        indexes = {
+                @Index(name = "idx_projects_updated_at_desc", columnList = "updated_at DESC, deleted_at"),
+                @Index(name = "idx_projects_deleted_at_updated_at_desc", columnList = "deleted_at, updated_at DESC"),
+                @Index(name = "idx_project_deleted_at", columnList = "deleted_at")
+        }
+)
+
+// order matters in indexing - updated_at DESC, deleted_at / deleted_at, updated_at DESC
+// put the column which would get filter out more records  ( updated_at DESC  - ( Field A ), deleted_at  - ( Field A ))
+// select * from projects where userid = ? and status = 'completed' - userid = ? would filter out most of the records
 public class Project {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
