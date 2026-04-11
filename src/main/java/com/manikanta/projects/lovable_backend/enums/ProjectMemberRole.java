8af336ca6492de042f8fb17e6567cfeb87a2bd60
Manikanta Parameswarappa <manikantap036@gmail.com>
2026-04-12 02:17:56 +0530

    add-project-permission-logic

--- src/main/java/com/manikanta/projects/lovable_backend/enums/ProjectMemberRole.java
+++ src/main/java/com/manikanta/projects/lovable_backend/enums/ProjectMemberRole.java
@@ -1,7 +1,21 @@
 package com.manikanta.projects.lovable_backend.enums;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
+import static com.manikanta.projects.lovable_backend.enums.ProjectPermission.*;
+
+import java.util.Set;
+
+@RequiredArgsConstructor
+@Getter
 public enum ProjectMemberRole {
-    EDITOR,
-    VIEWER,
-    OWNER
+    EDITOR(VIEW, EDIT, DELETE, VIEW_MEMBERS),
+    VIEWER(Set.of(VIEW, VIEW_MEMBERS)),
+    OWNER(Set.of(VIEW, EDIT, DELETE, MANAGE_MEMBERS, VIEW_MEMBERS));
+
+    ProjectMemberRole(ProjectPermission... permissions) {
+        this.permissions = Set.of(permissions);
+    }
+
+    private final Set<ProjectPermission> permissions;
 }
