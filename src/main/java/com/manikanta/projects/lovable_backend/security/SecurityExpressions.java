94471b7a094974f382dbd92337a850333d3230c9
Manikanta Parameswarappa <manikantap036@gmail.com>
2026-04-12 02:30:00 +0530

    refactor-and-add-some-more-permissions

--- src/main/java/com/manikanta/projects/lovable_backend/security/SecurityExpressions.java
+++ src/main/java/com/manikanta/projects/lovable_backend/security/SecurityExpressions.java
@@ -15,19 +15,31 @@ public class SecurityExpressions {
     ProjectMemberRepository projectMemberRepository;
     AuthUtil authUtil;
 
-    public boolean canViewProject(Long projectId) {
+    private boolean hasPermission(Long projectId, ProjectPermission projectPermission) {
         Long userId = authUtil.getCurrentUserId();
 
         return projectMemberRepository.findRoleByProjectIdAndUserId(projectId, userId).
-                map(role -> role.getPermissions().contains(ProjectPermission.VIEW))
+                map(role -> role.getPermissions().contains(projectPermission))
                 .orElse(false);
     }
 
-    public boolean canEditProject(Long projectId) {
-        Long userId = authUtil.getCurrentUserId();
+    public boolean canViewProject(Long projectId) {
+        return hasPermission(projectId, ProjectPermission.VIEW);
+    }
 
-        return projectMemberRepository.findRoleByProjectIdAndUserId(projectId, userId).
-                map(role -> role.getPermissions().contains(ProjectPermission.EDIT))
-                .orElse(false);
+    public boolean canEditProject(Long projectId) {
+        return hasPermission(projectId, ProjectPermission.EDIT);
+    }
+
+    public boolean canDeleteProject(Long projectId) {
+        return hasPermission(projectId, ProjectPermission.DELETE);
+    }
+
+    public boolean canViewMembers(Long projectId) {
+        return hasPermission(projectId, ProjectPermission.VIEW_MEMBERS);
+    }
+
+    public boolean canManageMembers(Long projectId) {
+        return hasPermission(projectId, ProjectPermission.MANAGE_MEMBERS);
     }
 }
